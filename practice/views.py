0ee5377d61aababf947c0652ccd53c93967016ac
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-30 15:44:49 -0500

    Initial porject

--- practice/views.py
+++ practice/views.py
@@ -2,6 +2,13 @@ from django.views.generic import TemplateView
 
 class FilterView(TemplateView):
     template_name = 'practice/filters.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = 'Webucator'
+        context['url'] = 'https://www.webucator.com'
+
+        return context
 
 
 class HomePageView(TemplateView):
@@ -10,3 +17,14 @@ class HomePageView(TemplateView):
 
 class TagView(TemplateView):
     template_name = 'practice/tags.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['beatles'] = [
+            {'firstname': 'Paul', 'lastname': 'McCartney'},
+            {'firstname': 'John', 'lastname': 'Lennon'},
+            {'firstname': 'George', 'lastname': 'Harrison'},
+            {'firstname': 'Ringo', 'lastname': 'Starr'},
+        ]
+    
+        return context
