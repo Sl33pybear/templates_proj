260a0b9ea1c9cb681451ec0d2dab977f2f4d390b
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-31 14:34:42 -0500

    Initial project

--- common/templatetags/common_filters.py
+++ common/templatetags/common_filters.py
@@ -1,3 +1,5 @@
+import random
+import re
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -7,3 +9,12 @@ register = template.Library()
 @stringfilter
 def repeat(value, times=2):
     return value * times
+
+@register.filter
+def clean(value):
+    cusses = ['stupid', 'stinky', 'darn', 'shucks', 'crud', 'dirt']
+    for cuss in cusses:
+        cuss_re = re.compile(re.escape(cuss), re.IGNORECASE)
+        chars = ''.join([random.choice('!@#$%^&*') for letter in cuss])
+        value = cuss_re.sub(chars, value)
+    return value
