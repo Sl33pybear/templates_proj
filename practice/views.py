2fac79152b0dfc0c8074e5430b57b7ecac84f34f
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-31 15:17:23 -0500

    Initial project

--- practice/views.py
+++ practice/views.py
@@ -21,52 +21,49 @@ class FilterView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['company'] = 'Webucator'
         context['url'] = 'https://www.webucator.com'
+        context['user_blurb'] = """Shucks! What a cruddy day I\'ve had.
+        I spent the whole darn day with my dirtiest
+        friend darning his STINKY socks."""
         context['moon_landing'] = datetime.datetime(
             year=1969, month=7, day=21,
             hour=2, minute=56, second=15,
             tzinfo=datetime.timezone.utc
         )
+        context['launch_date'] = datetime.datetime(
+            year=1969, month=7, day=16, hour=13, minute=32, second=0,
+            tzinfo=datetime.timezone.utc
+        )
+        context['century22'] = datetime.datetime(
+            year=2100, month=1, day=1, tzinfo=datetime.timezone.utc
+        )
 
-        return context
+        context['inventory'] = {
+            'gloves': 0,
+            'hats': 51,
+            'scarves': 2,
+            'socks': 13
+        }
+
+        context['classes'] = {
+            'Python': [
+                'Introduction to Python', 'Advanced Python',
+                'Data Science', 'Django'
+            ],
+            'Databases': [
+                'Introduction to PostgreSQL', 'Introduction to MySQL',
+                'Introduction to SQL Server', 'Introduction to Oracle'
+            ],
+            'Web': [
+                'HTML', 'CSS', 'JavaScript'
+            ],
+            'XML': [
+                'Introduction to XML'
+            ]
+        }
+
+        context['blurb'] = '<p>You are <em>pretty</em> smart!</p>'
+return context
     
-context['launch_date'] = datetime.datetime(
-    year=1969, month=7, day=16, hour=13, minute=32, second=0,
-    tzinfo=datetime.timezone.utc
-)
-
-context['century22'] = datetime.datetime(
-    year=2100, month=1, day=1, tzinfo=datetime.timezone.utc
-)
-
-context['inventory'] = {
-    'gloves': 0,
-    'hats': 51,
-    'scarves': 2,
-    'socks': 13
-}
-
-context['classes'] = {
-    'Python': [
-        'Introduction to Python', 'Advanced Python',
-        'Data Science', 'Django'
-    ],
-    'Databases': [
-        'Introduction to PostgreSQL', 'Introduction to MySQL',
-        'Introduction to SQL Server', 'Introduction to Oracle'
-    ],
-    'Web': [
-        'HTML', 'CSS', 'JavaScript'
-    ],
-    'XML': [
-        'Introduction to XML'
-    ]
-}
-
-context['blurb'] = '<p>You are <em>pretty</em> smart!</p>'
-
-context['user_blurb'] = """Shucks! What a cruddy day I\'ve had.
-I spent the whole darn day with my dirtiest
-friend darning his STINKY socks."""
 
 
 class HomePageView(TemplateView):
