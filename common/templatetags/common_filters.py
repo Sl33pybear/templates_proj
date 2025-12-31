858c2f9f17d426a843c618e0a057e660d7ca691c
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-31 14:32:59 -0500

    Initial project

--- common/templatetags/common_filters.py
+++ common/templatetags/common_filters.py
@@ -1,7 +1,9 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter
+@stringfilter
 def repeat(value, times=2):
     return value * times
